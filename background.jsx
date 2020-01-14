a3cc0428b49db6663a34d65789ad65651d0ce030
zzzeyez <zzzeyez@gmail.com>
2020-01-14 10:43:07 -0700

    switched to javascript from coffeescript

--- background.jsx
+++ background.jsx
@@ -1,7 +1,5 @@
-const command = 'date +\"%a, %b %d\"';
 const refreshFrequency = false; // ms
 
-const render = ({ output }) => <div class='screen'><link rel="stylesheet" type="text/css" href="/pecan/style.css" /><div class='pecanbackground' /	></div>;
+const render = () => <div class='screen'><link rel="stylesheet" type="text/css" href="/pecan/style.css" /><div class='pecanbackground' /	></div>;
 
-export { command, refreshFrequency, render };
-
+export { refreshFrequency, render };
