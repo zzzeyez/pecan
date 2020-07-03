711d0dcb59f9456b0ac78bf24feced35e180ba36
Naomi Wilkins <naomi@nwilkins.com>
2020-07-03 04:45:22 -0700

    simplified and improved stability of network widget

--- network.jsx
+++ network.jsx
@@ -1,6 +1,19 @@
 const command = "bash pecan/scripts/network";
 const refreshFrequency = 5000; // ms
 
-const render = ({ output }) => <div class='screen'><div class='pecannetwork'>{`${output}`}</div></div>;
+const render = ({ output, error }) => {
+    if (error) {
+        return (<div>{String(error)}</div>)
+    }
+
+    const up_down = output.match(/\d*\.\d*/g);
+    return (
+        <div class='screen'>
+            <div class='pecannetwork'>
+                ↓ {up_down[0]} ↑ {up_down[1]}
+            </div>
+        </div>
+    );
+};
 
 export { command, refreshFrequency, render };
